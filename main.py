6d307898412caf857564ec465eb1f023bf2bb5cf
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2021-02-28 15:43:13 +0530

    Updated Filter Words in DA

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ class Meeting:
     :return: None
   """
   def preprocess(self):
-    print(self.transcript)
     for transcript in self.transcript:
       # replace consecutive unigrams with a single instance
       transcript['segment'] = re.sub('\\b(\\w+)\\s+\\1\\b', '\\1', transcript['segment'])
@@ -39,6 +38,14 @@ class Meeting:
       transcript['segment'] = re.sub(' +', ' ', transcript['segment'])
       # strip leading and trailing white space
       transcript['segment'] = transcript['segment'].strip()
+
+      # remove filler words
+      with open('filters/filter_words.txt', 'r+') as f:
+        filler_words = f.read().splitlines()
+      transcript['segment'] = ' ' + transcript['segment'] + ' '
+      for filler_word in filler_words:
+        transcript['segment'] = re.sub(' ' + filler_word + ' ', ' ', transcript['segment'])
+        transcript['segment'] = re.sub(' ' + filler_word.capitalize() + ' ', ' ', transcript['segment'])
   
 """
   Get All Dataset's Meeting IDs
@@ -53,6 +60,6 @@ for meeting_id in meeting_ids:
   meeting = Meeting(meeting_id)
   meeting.get_transcript()
   meeting.preprocess()
-  print(meeting.get_transcript())
+  # print(meeting.get_transcript())
 
 
