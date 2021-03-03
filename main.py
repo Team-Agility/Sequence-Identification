1423b795896200b2a48d9f3d9dd3ca202e96f52e
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2021-03-03 23:02:33 +0530

    Implemented Stopword Removing on DA

--- main.py
+++ main.py
@@ -32,28 +32,40 @@ class Meeting:
   """
   def preprocess(self):
     print(f'{self.meeting_id}: Pre Processing...')
+
+    
+    with open('filters/filter_words.txt', 'r') as f:
+      filler_words = [filler_word.strip().lower() for filler_word in set(f.read().splitlines())]
+    with open('filters/stopwords.txt', 'r') as f:
+        stopwords = [stopword.strip().lower() for stopword in set(f.read().splitlines())]
+
     for transcript in self.transcript:
       # replace consecutive unigrams with a single instance
       transcript['segment'] = re.sub('\\b(\\w+)\\s+\\1\\b', '\\1', transcript['segment'])
       # same for bigrams
       transcript['segment'] = re.sub('(\\b.+?\\b)\\1\\b', '\\1', transcript['segment'])
-      # strip extra white space
-      transcript['segment'] = re.sub(' +', ' ', transcript['segment'])
-      # strip leading and trailing white space
-      transcript['segment'] = transcript['segment'].strip()
 
       # remove filler words
-      with open('filters/filter_words.txt', 'r+') as f:
-        filler_words = f.read().splitlines()
       transcript['segment'] = ' ' + transcript['segment'] + ' '
       for filler_word in filler_words:
         transcript['segment'] = re.sub(' ' + filler_word + ' ', ' ', transcript['segment'])
         transcript['segment'] = re.sub(' ' + filler_word.capitalize() + ' ', ' ', transcript['segment'])
+
+      # remove stopwords
+      transcript['segment'] = ' ' + transcript['segment'] + ' '
+      for stopword in stopwords:
+        transcript['segment'] = re.sub(' ' + stopword + ' ', ' ', transcript['segment'])
+        transcript['segment'] = re.sub(' ' + stopword.capitalize() + ' ', ' ', transcript['segment'])
+        
+      # strip extra white space
+      transcript['segment'] = re.sub(' +', ' ', transcript['segment'])
+      # strip leading and trailing white space
+      transcript['segment'] = transcript['segment'].strip()
   
 """
   Get All Dataset's Meeting IDs
   
-  :return: Strring Array with Meeting IDs
+  :return: String Array with Meeting IDs
 """
 def GetAllMeetingIDs():
   files = [os.path.basename(folder_path) for folder_path in glob.glob(f'{DATASET_OUT_DIR}/*')]
@@ -68,6 +80,7 @@ for meeting_id in meeting_ids:
   meeting = Meeting(meeting_id)
   meeting.get_transcript()
   meeting.preprocess()
-  # print(meeting.get_transcript())
+  print(meeting.get_transcript())
+  break
 
 
